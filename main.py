45386a18352f1e28d16ec30fc59024f59a52d79f
AznManDown <aznmandown@gmail.com>
2020-01-17 20:41:14 -0800

    Resolved issues with output not making use of calculate_lines and calculate coordinates.

--- main.py
+++ main.py
@@ -3,34 +3,28 @@ import imutils
 import argparse
 import numpy as np
 
-def calculate_lines(frames, lines):
+def calculate_lines(frame, lines):
     left = []
     right = []
-
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
-        parameters = np.polyfit((x1,y1), (x2, y2), 1)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
         slope = parameters[0]
-
         y_intercept = parameters[1]
-
-        if slope <0:
+        if slope < 0:
             left.append((slope, y_intercept))
         else:
             right.append((slope, y_intercept))
-
     left_avg = np.average(left, axis = 0)
     right_avg = np.average(right, axis = 0)
-
     left_line = calculate_coordinates(frame, left_avg)
     right_line = calculate_coordinates(frame, right_avg)
-
     return np.array([left_line, right_line])
 
 def calculate_coordinates(frame, parameters):
     slope, intercept = parameters
     y1 = image.shape[0]
-    y2 = int(y1 - 150)
+    y2 = int(y1 - 500)
 
     x1 = int((y1 - intercept) / slope)
     x2= int((y2 - intercept) / slope)
@@ -40,7 +34,7 @@ def calculate_coordinates(frame, parameters):
 
 
 # Load test image from Dashcam
-image = cv2.imread("test_images/out9.png", -1)
+image = cv2.imread("test_images/out2.png")
 
 # Grayscale image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -58,10 +52,12 @@ masked_image = cv2.bitwise_and(eConvert, mask)
 
 hough = cv2.HoughLinesP(masked_image, 2, np.pi / 180, 100, np.array([]), 100, 50)
 
+lines = calculate_lines(image, hough)
+
 lines_visualize = np.zeros_like(image)
 
-for line in hough:
-    for x1, y1, x2, y2 in line:
+if lines is not None:
+    for x1, y1, x2, y2 in lines:
         cv2.line(lines_visualize, (x1, y1), (x2, y2), (0, 255, 0), 15)
 
 output = cv2.addWeighted(image, 0.9, lines_visualize, 1, 1)
